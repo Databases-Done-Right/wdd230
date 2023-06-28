6329cdaa29918c439c931cd90df99b44b9cf7ce5
Databases-Done-Right <consulting@databasesdoneright.com>
2023-06-28 07:24:27 -0600

    W04 Team Activity: Form Build

---
+++ scripts/form.js
@@ -0,0 +1,27 @@
+const kp1 = document.querySelector("#passwordField");
+const kp2 = document.querySelector("#reEnterPasswordField");
+const message = document.querySelector("#formmessage");
+
+kp2.addEventListener("focusout", checkSame);
+
+// This should be refactored.
+function checkSame() {
+	if (kp1.value !== kp2.value) {
+		message.textContent = "❗Passwords DO NOT MATCH!";
+		message.style.display = "block";
+        kp2.classList.add("invalid");
+	} else {
+        kp2.classList.remove("invalid");
+		message.style.display = "none";
+	}
+}
+
+const range = document.querySelector("#pageRatingField");
+const rangeValue = document.querySelector("#pageRatingTextField");
+
+range.addEventListener('change', displayRatingValue);
+range.addEventListener('input', displayRatingValue);
+
+function displayRatingValue() {
+    rangeValue.value = range.value;
+}
--- scripts/getDates.js
+++ scripts/getDates.js
@@ -2,5 +2,8 @@ document.querySelector("#year").innerHTML = new Date().getFullYear();
 document.querySelector("#lastModified").innerHTML = "Last Modified: " + document.lastModified;
 let pageVisits = localStorage.getItem('pageCounter') || 0;
 pageVisits++;
-document.querySelector("#pageCounter").innerHTML = pageVisits;
+let pageVisitElement = document.querySelector("#pageCounter");
+if(pageVisitElement) {
+    pageVisitElement.innerHTML = pageVisits;
+}
 localStorage.setItem('pageCounter', pageVisits);
